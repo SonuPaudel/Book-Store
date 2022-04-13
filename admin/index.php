98184491813fc8841961aafaa8adbd8484545c39
Sonu Paudel <86399801+SonuPaudel@users.noreply.github.com>
2022-04-13 13:36:46 +0545

    added files

---
+++ admin/index.php
@@ -0,0 +1,1 @@
+echo "hello sonu vonu"
---
+++ templates/db_conn.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$sname = "localhost";
+$unmae = "root";
+$password = "";
+$db_name = "book-store";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+}
